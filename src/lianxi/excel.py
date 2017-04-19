260e92331fab9f09281fb4827e24667a6dc1c301
zww <snczww@hotmail.com>
2017-04-19 08:54:26 +0800

    遇到路径问题，相对路径

--- src/lianxi/excel.py
+++ src/lianxi/excel.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 '''
 Created on 2017年4月18日
 
@@ -5,8 +6,8 @@ Created on 2017年4月18日
 '''
 import pandas as pd
 
-inputfile = '../data/huizong.csv'
-outputfile = '../data/haier_jd.csv'
-data = pd.read_csv(inputfile,encoding = 'utf-8')
+inputfile = 'C:\\Users\\zww\\Desktop\\lunwen\\chapter15\\demo\\data\\huizong.csv'
+outputfile = 'D:\\源码\\python\\bishe\\src\\lianxi\\data\\haier_jd.txt'
+data = pd.read_csv(inputfile, encoding = 'utf-8')
 data = data[[u'评论']][data[u'品牌']==u'海尔']
-data.to_csv(outputfile, index=False, header=False, encoding='utf-8')
+data.to_csv(outputfile, index=False, header=False,encoding='utf-8')
